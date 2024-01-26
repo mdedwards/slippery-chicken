2b330248df8637a2adc080b0fad79e3790e74331
Michael Edwards <michael.edwards@folkwang-uni.de>
2024-01-26 18:33:19 +0100

    fixed assoc-list::add problem with (setf (data...

--- doc/dot-swank.lisp
+++
@@ -1,7 +0,0 @@
-(load "/lisp/cmn/cmn-all.lisp")
-(load "/lisp/clm-4/all.lisp")
-(load "/Users/medward2/lisp/sc/src/all.lsp")
-
-(in-package :clm)
-(compile-file "/lisp/clm-4/nrev.ins")
-(load "/lisp/clm-4/nrev.dx32fsl")
